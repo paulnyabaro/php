a469f904a0377a15a068f4b2fe130e610be495f4
paulnyabaro <paulnyabaro1@gmail.com>
2024-10-28 06:44:47 +0300

    Working with varible scope

--- index.php
+++ index.php
@@ -40,6 +40,7 @@ myTest();
 echo $y; // outputs 15
 
 
+// PHP also stores all global variables in an array called $GLOBALS[index]. The index holds the name of the variable. This array is also accessible from within functions and can be used to update global variables directly.
 
 ?>
 
