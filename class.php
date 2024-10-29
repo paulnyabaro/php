75aae821c7b45c0af4f65fb4130458e78b2c784c
paulnyabaro <paulnyabaro1@gmail.com>
2024-10-29 06:56:43 +0300

    Working with classes

---
+++ class.php
@@ -0,0 +1,21 @@
+<?php 
+
+class Car{
+    public $color;
+    public $model;
+    public $make;
+
+    public function __construct($color, $model, $make){
+        $this->color = $color;
+        $this->model = $model;
+        $This->make = $make;
+    }
+    public function message(){
+        return 'My car is ' . $this->color . ' ' . $this->model . ' '. $this->make . '!';
+    }
+}
+
+$myCar = new Car('white', 'GLE', 'Mercedez Benz');
+var_dump($myCar)
+
+?>
