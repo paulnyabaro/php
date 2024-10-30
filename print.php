a94976c6d1ae035b013e31fbb5c59478ffd7fd8d
paulnyabaro <paulnyabaro1@gmail.com>
2024-10-30 09:22:55 +0300

    Working with constants

--- print.php
+++ print.php
@@ -10,6 +10,16 @@ echo var_dump(is_numeric($x));
 // casting to string
 $age = 23;
 $age = (string) $age;
-var_dump($age)
+var_dump($age);
+echo(abs(-6.7));
+echo(round(0.60));
+echo(round(0.49));
+echo(rand(15, 40));
+
+
+// Constants
+define("GREETING", "<br>Welcome to W3Schools.com!");
+echo GREETING
+
 
 ?>
