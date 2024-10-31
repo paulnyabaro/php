782592a665fe8d450c6a388716f44e2bec16d9f2
paulnyabaro <paulnyabaro1@gmail.com>
2024-10-31 08:00:32 +0300

    Working with switch

--- switch.php
+++ switch.php
@@ -1,18 +1,47 @@
 <?php
 
 // Switch Statements
-switch (expression) {
-    case label1:
-      //code block
-      break;
-    case label2:
-      //code block;
-      break;
-    case label3:
-      //code block
-      break;
+// switch (expression) {
+//     case label1:
+//       //code block
+//       break;
+//     case label2:
+//       //code block;
+//       break;
+//     case label3:
+//       //code block
+//       break;
+//     default:
+//       //code block
+//   }
+
+
+// //   Example
+// $favcolor = "red";
+
+// switch ($favcolor) {
+//   case "red":
+//     echo "Your favorite color is red!";
+//     break;
+//   case "blue":
+//     echo "Your favorite color is blue!";
+//     break;
+//   case "green":
+//     echo "Your favorite color is green!";
+//     break;
+//   default:
+//     echo "Your favorite color is neither red, blue, nor green!";
+// }
+
+$age = 30;
+
+switch ($age){
+    case 27:
+        echo 'You are below 30 years';
+        break;
+    
     default:
-      //code block
-  }
+        echo 'You are 30 and above';
+    }
 
 ?>
