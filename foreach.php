4d71352f300b9b6632a9226dcf62ae1f545358a9
paulnyabaro <paulnyabaro1@gmail.com>
2024-11-01 12:57:52 +0300

    Working with foreach and arrays

--- foreach.php
+++ foreach.php
@@ -3,7 +3,7 @@
 $members = array("peter" => "3", "ken" => "12", "jan"=> "5");
 
 foreach ($members as $member => $position){
-    echo ("$member". ":" . $position . '<br />');
+    echo ("$member : $position <br />");
 }
 
 ?>
