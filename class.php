cc0d399ae9f049adb7620c848fd382904429b5b6
paulnyabaro <paulnyabaro1@gmail.com>
2024-11-04 10:16:48 +0300

    Added days left feature

--- class.php
+++ class.php
@@ -4,21 +4,21 @@ class Car{
     public $color;
     public $model;
     public $make;
+    public $year;
 
-    public function __construct($color, $model, $make){
+    public function __construct($color, $model, $make, $year){
         $this->color = $color;
         $this->model = $model;
         $this->make = $make;
+        $this->year = $year;
     }
     public function message(){
-        return 'My car is ' . $this->color . ' ' . $this->model . ' '. $this->make . '!';
+        return 'My car is a ' . $this->year . '' . $this->color . ' ' . $this->model . ' '. $this->make . '!';
     }
 }
 
-$myCar = new Car('white', 'GLE', 'Mercedez Benz');
+$myCar = new Car('white', 'GLC', 'Mercedez Benz', '2018');
 var_dump($myCar);
-echo("$myCar");
-
 
 
 ?>
