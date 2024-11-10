d14aa8b9277499f3a44ec0e5b63d3b2a9c5ed30b
paulnyabaro <paulnyabaro1@gmail.com>
2024-11-10 17:55:00 +0300

    Working with array splice

--- arrays.php
+++ arrays.php
@@ -7,4 +7,14 @@ array_push($fruits, "Lemon", "banana");
 
 var_dump($fruits);
 
+
+// Adding to associative array
+$cars = array("brand" => "Ford", "model" => "Mustang");
+$cars += ["color" => "red", "year" => 1964];
+
+
+// Deleting array items with array_splice
+
+array_splice($cars, 1, 1);
+
 ?>
