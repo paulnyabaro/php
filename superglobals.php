f1e18fd4b568bf6009c662bab1b5960f6112cdcc
paulnyabaro <paulnyabaro1@gmail.com>
2024-11-11 05:59:11 +0300

    Working with Server global

--- superglobals.php
+++ superglobals.php
@@ -14,4 +14,41 @@
 // $_COOKIE
 // $_SESSION
 
+// GLOBALS
+$x = 75;
+  
+function myfunction() {
+  echo $GLOBALS['x'];
+}
+
+myfunction()
+
+
+// or 
+
+// $x = 75;
+  
+// function myfunction() {
+//   global $x;
+//   echo $x;
+// }
+
+// myfunction()
+
+
+// $_SERVER
+
+echo $_SERVER['PHP_SELF'];
+echo "<br>";
+echo $_SERVER['SERVER_NAME'];
+echo "<br>";
+echo $_SERVER['HTTP_HOST'];
+echo "<br>";
+echo $_SERVER['HTTP_REFERER'];
+echo "<br>";
+echo $_SERVER['HTTP_USER_AGENT'];
+echo "<br>";
+echo $_SERVER['SCRIPT_NAME'];
+
+
 ?>
