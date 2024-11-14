32e0c28c5987842e70fadd5aa0f0918c72ca97f8
paulnyabaro <paulnyabaro1@gmail.com>
2024-11-14 07:05:43 +0300

    Using preg_match all

--- regex.php
+++ regex.php
@@ -5,4 +5,11 @@ $pattern = "/w3schools/i";
 
 echo preg_match($pattern, $str);
 
+
+
+// Using preg_match_all()
+$str1 = "The rain in SPAIN falls mainly on the plains.";
+$pattern = "/ain/i";
+echo preg_match_all($pattern, $str1);
+
 ?>
