f8c504306414af23b13e5406ccceea6c493a25ba
paulnyabaro <paulnyabaro1@gmail.com>
2024-11-15 07:53:06 +0300

    Working on data validation

--- data-validation.php
+++ data-validation.php
@@ -1,3 +1,16 @@
+<form action="">
+    Name: <input type="text" name="name">
+    E-mail: <input type="text" name="email">
+    Website: <input type="text" name="website">
+    Comment: <textarea name="comment" rows="5" cols="40"></textarea>
+
+    Gender:
+    <input type="radio" name="gender" value="female">Female
+    <input type="radio" name="gender" value="male">Male
+    <input type="radio" name="gender" value="other">Other
+</form>
+
+
 <?php
 
 // define variables and set to empty values
