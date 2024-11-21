ba2fc9cf3337c4846387f141f259f42dc20e4427
paulnyabaro <paulnyabaro1@gmail.com>
2024-11-21 08:53:23 +0300

    Working with dates

--- date.php
+++ date.php
@@ -8,6 +8,6 @@ echo "Today is " . date("l"). "<br />";
 date_default_timezone_set("Africa/Nairobi");
 echo "The time is " . date("h:i:sa"). "<br />";
 
-$d=mktime(11, 14, 54, 8, 12, 2014);
+$d=mktime(8, 52, 54, 11, 21, 2024);
 echo "Created date is " . date("Y-m-d h:i:sa", $d);
 ?>
