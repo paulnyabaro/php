63cf5ef69cccab432badf0315f7d72e418cb01a0
paulnyabaro <paulnyabaro1@gmail.com>
2024-11-21 08:56:45 +0300

    Working with strtodate function

--- date.php
+++ date.php
@@ -9,5 +9,21 @@ date_default_timezone_set("Africa/Nairobi");
 echo "The time is " . date("h:i:sa"). "<br />";
 
 $d=mktime(8, 52, 54, 11, 21, 2024);
-echo "Created date is " . date("Y-m-d h:i:sa", $d);
+echo "Created date is " . date("Y-m-d h:i:sa", $d). "<br />";
+
+
+// Using strtotime() to create time
+$d=strtotime("10:30pm April 15 2014");
+echo "Created date is " . date("Y-m-d h:i:sa", $d). "<br />";
+
+
+$d=strtotime("tomorrow");
+echo date("Y-m-d h:i:sa", $d) . "<br>";
+
+$d=strtotime("next Saturday");
+echo date("Y-m-d h:i:sa", $d) . "<br>";
+
+$d=strtotime("+3 Months");
+echo date("Y-m-d h:i:sa", $d) . "<br>";
+
 ?>
