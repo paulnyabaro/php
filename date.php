88b98b1e1195ceb33d1bbb3708df7037d09f801c
paulnyabaro <paulnyabaro1@gmail.com>
2024-11-21 08:51:58 +0300

    Working with dates

--- date.php
+++ date.php
@@ -2,5 +2,12 @@
 echo "Today is " . date("Y/m/d") . "<br>";
 echo "Today is " . date("Y.m.d") . "<br>";
 echo "Today is " . date("Y-m-d") . "<br>";
-echo "Today is " . date("l");
+echo "Today is " . date("l"). "<br />";
+
+
+date_default_timezone_set("Africa/Nairobi");
+echo "The time is " . date("h:i:sa"). "<br />";
+
+$d=mktime(11, 14, 54, 8, 12, 2014);
+echo "Created date is " . date("Y-m-d h:i:sa", $d);
 ?>
